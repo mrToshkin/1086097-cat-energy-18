170440b78e686fc0913c8852230357ebfb05a16c
Leonov-nic <leonov-nic@yandex.ru>
2020-01-10 02:02:55 +0300

    9-января

--- source/js/slider.js
+++ source/js/slider.js
@@ -24,22 +24,22 @@ var after = document.querySelector(".example__item--after");
 var before = document.querySelector(".example__item--before");
 var ind = document.querySelector(".example__slider");
 
-toggleOne.addEventListener("click", function(evt) {
+toggleTwo.addEventListener("click", function(evt) {
 
   evt.preventDefault();
 
   before.classList.remove("example__image-none");
   after.classList.add("example__image-none");
-  ind.classList.add("example__slider-indicator");
-  ind.classList.remove("example__slider-indicator--two");
+  ind.classList.remove("example__slider-indicator");
+  ind.classList.add("example__slider-indicator--two");
 });
 
-toggleTwo.addEventListener("click", function(evt) {
+toggleOne.addEventListener("click", function(evt) {
 
   evt.preventDefault();
 
   before.classList.add("example__image-none");
   after.classList.remove("example__image-none");
-  ind.classList.remove("example__slider-indicator");
-  ind.classList.add("example__slider-indicator--two");
+  ind.classList.add("example__slider-indicator");
+  ind.classList.remove("example__slider-indicator--two");
 });
